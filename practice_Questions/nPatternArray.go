82e0bdd80243519ba28c0a95f077484c25754d54
Hemant Chaurasia <chaurasiahemant2002@gmail.com>
2023-10-30 20:48:25 +0530

    added: N pattern array program

--- practice_Questions/nPatternArray.go
+++ practice_Questions/nPatternArray.go
@@ -9,12 +9,16 @@ func main() {
 	fmt.Printf("Enter the value of n : ")
 	fmt.Scanf("%d", &n)
 	array := make([][]int, n)
-	var value int = 0
+	var value int = 1
 	for i := 0; i < n; i++ {
 		array[i] = make([]int, n)
 		for j := 0; j < n; j++ {
-			array[i][j] = value
+			if j >= n-i-1 {
+				array[i][j] = value
+				value++
+			}
 		}
+		value = 1
 	}
 	fmt.Println("2D array : ")
 	for i := 0; i < n; i++ {
