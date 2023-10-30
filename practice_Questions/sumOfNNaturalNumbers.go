a3bc4aa1deb071b2f718eed6dcba75cf34888e1f
Hemant Chaurasia <chaurasiahemant2002@gmail.com>
2023-10-30 12:53:34 +0530

    feat: some changes in sum of natural number file

--- practice_Questions/sumOfNNaturalNumbers.go
+++ practice_Questions/sumOfNNaturalNumbers.go
@@ -1,9 +1,17 @@
+//sum of N natural numbers (N is given by user)
 package main
 
 import (
 	"fmt"
 )
+//fastest approach
+func sum(n int) int {
+	var sum int
+	sum = n * (n + 1) / 2
+	return sum
+}
 
+//first try
 func sumOfN(n int) int {
 	var sum int
 	sum = 0
@@ -16,8 +24,11 @@ func sumOfN(n int) int {
 func main() {
 	var num int
 	var ans int
+	var secondAns int
 	fmt.Printf("Enter the value of N : ")
 	fmt.Scanf("%d", &num)
 	ans = sumOfN(num)
-	fmt.Printf("Sum of first %d natural numbers : %v", num, ans)
+	secondAns = sumOfN(num)
+	fmt.Printf("Sum of first %d natural numbers : %v\n", num, ans)
+	fmt.Printf("Sum of first %d natural numbers : %v", num, secondAns)
 }
